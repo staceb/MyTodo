654969e48f72e4d8e21196eb45ee4f3e2a877917
omokehinde <engr.omokehinde@gmail.com>
2019-06-23 05:10:19 +0100

    Todo items component

--- src/App.js
+++ src/App.js
@@ -3,34 +3,42 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
-  state={
-    todos: [
-      {
-        id: 1,
-        title: 'Take out to the trash',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Take a walk',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Eat out with Moyo',
-        completed: false
-      }
-    ]
-  }
-  
+    state={
+        todos: [{
+                id: 1,
+                title: 'Take out to the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Take a walk',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'Eat out with Moyo',
+                completed: false
+            }
+        ]
+    }
+
+    markComplete=(id)=>{
+      this.setState({ todos:this.state.todos.map(todo=>{
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      }) });
+    }
+
     render() {
-      console.log(this.state.todos)
-        return ( 
-          <div className = "App">
-            <Todos />
+        return (
+          <div className = "App" >
+            <Todos todos={this.state.todos} markComplete={this.markComplete} / >
             </div>
         );
     }
 }
 
 export default App;
+``
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import Todoitem from './Todoitem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
     render() {
-        return (
-          <div>
-            <h1>Todos</h1>
-            </div>
-        );
+      return this.props.todos.map((todo)=>{
+        return <Todoitem key={todo.id} todo={todo} markComplete={this.props.markComplete}/>
+      });
+      // return <h1>{2+44}</h1>
     }
+}
+
+Todos.PropTypes={
+  todos: PropTypes.object.isRequired
 }
 
 export default Todos;
